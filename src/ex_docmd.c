7225d1c43be007cd56d4e9285f74c68465386bf0
Bram Moolenaar <bram@vim.org>
1999-12-21 19:00:16 +0000

    Patch 5.6a.004 Problem:    A missing ":endwhile" or ":endif" doesn't give an error message.             (Johannes Zellner) Solution:   Check for missing ":endwhile" and ":endif" in sourced files.             Add missing ":endif" in file selection macros. Files:      src/ex_docmd.c, runtime/macros/file_select.vim

--- src/ex_docmd.c
+++ src/ex_docmd.c
@@ -581,6 +581,14 @@ do_cmdline(cmdline, getline, cookie, flags)
 #ifdef WANT_EVAL
     free_cmdlines(&lines_ga);
     ga_clear(&lines_ga);
+
+    if (cstack.cs_idx >= 0 && getline == getsourceline)
+    {
+	if (cstack.cs_flags[cstack.cs_idx] & CSF_WHILE)
+	    EMSG("Missing :endwhile");
+	else
+	    EMSG("Missing :endif");
+    }
 #endif
 
     /*
