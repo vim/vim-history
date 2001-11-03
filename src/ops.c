952816982cdd1f1a9b6445a360d6c9888ee1f257
Bram Moolenaar <bram@vim.org>
2001-11-03 20:37:23 +0000

    Patch 6.0.068 Problem:    When formatting a Visually selected area with "gq" and the number 	    of lines increases the last line may not be redrawn correctly. 	    (Yegappan Lakshmanan) Solution:   Correct the area to be redrawn for inserted/deleted lines. Files:	    src/ops.c

--- src/ops.c
+++ src/ops.c
@@ -4034,10 +4034,31 @@ op_format(oap)
     if (oap->end_adjusted && curwin->w_cursor.lnum < curbuf->b_ml.ml_line_count)
 	++curwin->w_cursor.lnum;
     beginline(BL_WHITE | BL_FIX);
-    msgmore(curbuf->b_ml.ml_line_count - old_line_count);
+    old_line_count = curbuf->b_ml.ml_line_count - old_line_count;
+    msgmore(old_line_count);
 
     /* put '] mark on the end of the formatted area */
     curbuf->b_op_end = curwin->w_cursor;
+
+#ifdef FEAT_VISUAL
+    if (oap->is_VIsual)
+    {
+	win_T	*wp;
+
+	FOR_ALL_WINDOWS(wp)
+	{
+	    if (wp->w_old_cursor_lnum != 0)
+	    {
+		/* When lines have been inserted or deleted, adjust the end of
+		 * the Visual area to be redrawn. */
+		if (wp->w_old_cursor_lnum > wp->w_old_visual_lnum)
+		    wp->w_old_cursor_lnum += old_line_count;
+		else
+		    wp->w_old_visual_lnum += old_line_count;
+	    }
+	}
+    }
+#endif
 }
 
 /*
