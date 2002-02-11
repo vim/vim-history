810fda1b0c44af3f34472c966d4860b34359da07
Bram Moolenaar <bram@vim.org>
2002-02-11 12:56:13 +0000

    Patch 6.0.210 Problem:    After patch 6.0.167 it's no longer possible to edit a help file in 	    another encoding than latin1. Solution:   Let the "++enc=" argument overrule the encoding. Files:	    src/fileio.c

--- src/fileio.c
+++ src/fileio.c
@@ -670,15 +670,15 @@ readfile(fname, sfname, from, lines_to_skip, lines_to_read, eap, flags)
     /*
      * Decide which 'encoding' to use first.
      */
-    if (curbuf->b_help)
-    {
-	fenc = (char_u *)"latin1";	/* help files are latin1 */
-	fenc_alloced = FALSE;
-    }
-    else if (eap != NULL && eap->force_enc != 0)
+    if (eap != NULL && eap->force_enc != 0)
     {
 	fenc = enc_canonize(eap->cmd + eap->force_enc);
 	fenc_alloced = TRUE;
+    }
+    else if (curbuf->b_help)
+    {
+	fenc = (char_u *)"latin1";	/* help files are latin1 */
+	fenc_alloced = FALSE;
     }
     else if (curbuf->b_p_bin)
     {
