2d5addf958c46151306e040595403a01f51c05b0
Bram Moolenaar <bram@vim.org>
2003-04-14 19:15:11 +0000

    Patch 6.1.462 Problem:    When autocommands wipe out a buffer, a crash may happen. (Hari 	    Krishna Dara) Solution:   Don't decrement the window count of a buffer before calling the 	    autocommands for it.  When re-using the current buffer, watch out 	    for autocommands changing the current buffer. Files:	    src/buffer.c, src/ex_cmds.c, src/proto/buffer.pro

--- src/ex_cmds.c
+++ src/ex_cmds.c
@@ -2833,7 +2833,7 @@ do_ecmd(fnum, ffname, sfname, eap, newlnum, flags)
 	else
 	    new_name = NULL;
 #endif
-	buf_freeall(curbuf, FALSE);	/* free all things for buffer */
+	buf_freeall(curbuf, FALSE, FALSE);   /* free all things for buffer */
 #ifdef FEAT_AUTOCMD
 	/* If autocommands deleted the buffer we were going to re-edit, give
 	 * up and jump to the end. */
