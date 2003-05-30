65585e6040e02112c2b8a6857cd1966c06e038d9
Bram Moolenaar <bram@vim.org>
2003-05-30 16:05:55 +0000

    Patch 6.2f.025 Problem:    When reading a file from stdin that has DOS line endings but a 	    missing end-of-line for the last line 'fileformat' becomes "unix". 	    (Bill McCarthy) Solution:   Don't add the missing line break when re-reading the text from the 	    buffer. Files:	    src/fileio.c

--- src/fileio.c
+++ src/fileio.c
@@ -1109,6 +1109,10 @@ retry:
 				read_buf_col = 0;
 				if (++read_buf_lnum > from)
 				{
+				    /* When the last line didn't have an
+				     * end-of-line don't add it now either. */
+				    if (!curbuf->b_p_eol)
+					--tlen;
 				    size = tlen;
 				    break;
 				}
