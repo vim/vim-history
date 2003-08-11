240fec4ce3ba10ac1f812ae1e6181409dad210cd
Bram Moolenaar <bram@vim.org>
2003-08-11 06:08:40 +0000

    Patch 6.2.064 Problem:    resolve() only handles one symbolic link, need to repeat it to 	    resolve all of them.  Then need to simplify the file name. Solution:   Make resolve() resolve all symbolic links and simplify the result. 	    Add simplify() to just simplify a file name.  Fix that test49 	    doesn't work if /tmp is a symbolic link.  (Servatius Brandt) Files:	    runtime/doc/eval.txt, src/eval.c, src/tag.c, 	    src/testdir/test49.vim

--- src/eval.c
+++ src/eval.c
@@ -318,6 +318,7 @@ static void f_server2client __ARGS((VAR argvars, VAR retvar));
 static void f_serverlist __ARGS((VAR argvars, VAR retvar));
 static void f_setline __ARGS((VAR argvars, VAR retvar));
 static void f_setreg __ARGS((VAR argvars, VAR retvar));
+static void f_simplify __ARGS((VAR argvars, VAR retvar));
 static void find_some_match __ARGS((VAR argvars, VAR retvar, int start));
 static void f_strftime __ARGS((VAR argvars, VAR retvar));
 static void f_stridx __ARGS((VAR argvars, VAR retvar));
@@ -2812,6 +2813,7 @@ static struct fst
     {"setline",		2, 2, f_setline},
     {"setreg",		2, 3, f_setreg},
     {"setwinvar",	3, 3, f_setwinvar},
+    {"simplify",	1, 1, f_simplify},
 #ifdef HAVE_STRFTIME
     {"strftime",	1, 2, f_strftime},
 #endif
@@ -5808,6 +5810,7 @@ f_resolve(argvars, retvar)
     VAR		retvar;
 {
     char_u	*p;
+    int		limit = 100;
 
     p = get_var_string(&argvars[0]);
 #ifdef FEAT_SHORTCUT
@@ -5826,33 +5829,192 @@ f_resolve(argvars, retvar)
 	char_u	buf[MAXPATHL + 1];
 	char_u	*cpy;
 	int	len;
+	char_u	*remain = NULL;
+	char_u	*q;
+	int	is_relative_to_current = FALSE;
+	int	has_trailing_pathsep = FALSE;
 
-	len = readlink((char *)p, (char *)buf, MAXPATHL);
-	if (len > 0)
+	p = vim_strsave(p);
+
+	if (p[0] == '.' && (vim_ispathsep(p[1])
+				   || (p[1] == '.' && (vim_ispathsep(p[2])))))
+	    is_relative_to_current = TRUE;
+
+	len = STRLEN(p);
+	if (len > 0 && vim_ispathsep(p[len-1]))
+	    has_trailing_pathsep = TRUE;
+
+	q = getnextcomp(p);
+	if (*q != NUL)
 	{
-	    buf[len] = NUL;
-	    if (gettail(p) > p && !mch_isFullName(buf))
+	    /* Separate the first path component in "p", and keep the
+	     * remainder (beginning with the path separator). */
+	    remain = vim_strsave(q - 1);
+	    q[-1] = NUL;
+	}
+
+	for(;;)
+	{
+	    for (;;)
 	    {
-		/* symlink is relative to directory of argument */
-		cpy = alloc((unsigned)(STRLEN(p) + STRLEN(buf) + 1));
-		if (cpy != NULL)
+		len = readlink((char *)p, (char *)buf, MAXPATHL);
+		if (len <= 0)
+		    break;
+		buf[len] = NUL;
+
+		if (limit-- == 0)
 		{
-		    STRCPY(cpy, p);
-		    STRCPY(gettail(cpy), buf);
-		    retvar->var_val.var_string = cpy;
-		    p = NULL;
+		    vim_free(p);
+		    vim_free(remain);
+		    EMSG(_("E655: Too much symbolic links (cycle?)"));
+		    retvar->var_val.var_string = NULL;
+		    goto fail;
+		}
+
+		/* Ensure that the result will have a trailing path separator
+		 * if the argument has one. */
+		if (remain == NULL && has_trailing_pathsep)
+		    add_pathsep(buf);
+
+		/* Separate the first path component in the link value and
+		 * concatenate the remainders. */
+		q = getnextcomp(vim_ispathsep(*buf) ? buf + 1 : buf);
+		if (*q != NUL)
+		{
+		    if (remain == NULL)
+			remain = vim_strsave(q - 1);
+		    else
+		    {
+			cpy = vim_strnsave(q-1, STRLEN(q-1)+STRLEN(remain));
+			if (cpy != NULL)
+			{
+			    STRCAT(cpy, remain);
+			    vim_free(remain);
+			    remain = cpy;
+			}
+		    }
+		    q[-1] = NUL;
+		}
+
+		q = gettail(p);
+		if (q > p && *q == NUL)
+		{
+		    /* Ignore trailing path separator. */
+		    q[-1] = NUL;
+		    q = gettail(p);
+		}
+		if (q > p && !mch_isFullName(buf))
+		{
+		    /* symlink is relative to directory of argument */
+		    cpy = alloc((unsigned)(STRLEN(p) + STRLEN(buf) + 1));
+		    if (cpy != NULL)
+		    {
+			STRCPY(cpy, p);
+			STRCPY(gettail(cpy), buf);
+			vim_free(p);
+			p = cpy;
+		    }
+		}
+		else
+		{
+		    vim_free(p);
+		    p = vim_strsave(buf);
 		}
 	    }
+
+	    if (remain == NULL)
+		break;
+
+	    /* Append the first path component of "remain" to "p". */
+	    q = getnextcomp(remain + 1);
+	    len = q - remain - (*q != NUL);
+	    cpy = vim_strnsave(p, STRLEN(p) + len);
+	    if (cpy != NULL)
+	    {
+		STRNCAT(cpy, remain, len);
+		vim_free(p);
+		p = cpy;
+	    }
+	    /* Shorten "remain". */
+	    if (*q != NUL)
+		STRCPY(remain, q - 1);
 	    else
-		p = buf;
+	    {
+		vim_free(remain);
+		remain = NULL;
+	    }
 	}
-	if (p != NULL)
-	    retvar->var_val.var_string = vim_strsave(p);
+
+	/* If the result is a relative path name, make it explicitly relative to
+	 * the current directory if and only if the argument had this form. */
+	if (!vim_ispathsep(*p))
+	{
+	    if (is_relative_to_current
+		    && *p != NUL
+		    && !(p[0] == '.'
+			&& (p[1] == NUL
+			    || vim_ispathsep(p[1])
+			    || (p[1] == '.'
+				&& (p[2] == NUL
+				    || vim_ispathsep(p[2]))))))
+	    {
+		/* Prepend "./". */
+		cpy = vim_strnsave((char_u *)"./", 2 + STRLEN(p));
+		if (cpy != NULL)
+		{
+		    STRCAT(cpy, p);
+		    vim_free(p);
+		    p = cpy;
+		}
+	    }
+	    else if (!is_relative_to_current)
+	    {
+		/* Strip leading "./". */
+		q = p;
+		while (q[0] == '.' && vim_ispathsep(q[1]))
+		    q += 2;
+		if (q > p)
+		    mch_memmove(p, p + 2, STRLEN(p + 2) + (size_t)1);
+	    }
+	}
+
+	/* Ensure that the result will have no trailing path separator
+	 * if the argument had none.  But keep "/" or "//". */
+	if (!has_trailing_pathsep)
+	{
+	    q = p + STRLEN(p);
+	    while ((q > p + 2 || (q == p + 2 && !vim_ispathsep(*p)))
+		   && vim_ispathsep(q[-1]))
+		--q;
+	    *q = NUL;
+	}
+
+	retvar->var_val.var_string = p;
     }
 # else
     retvar->var_val.var_string = vim_strsave(p);
 # endif
 #endif
+
+    simplify_filename(retvar->var_val.var_string);
+
+fail:
+    retvar->var_type = VAR_STRING;
+}
+
+/*
+ * "simplify()" function
+ */
+    static void
+f_simplify(argvars, retvar)
+    VAR		argvars;
+    VAR		retvar;
+{
+    char_u	*p;
+
+    p = get_var_string(&argvars[0]);
+    retvar->var_val.var_string = vim_strsave(p);
+    simplify_filename(retvar->var_val.var_string);	/* simplify in place */
     retvar->var_type = VAR_STRING;
 }
 
