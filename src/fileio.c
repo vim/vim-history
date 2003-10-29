f9d43a0af3602c9909de5e845abf9f09454bd681
Bram Moolenaar <bram@vim.org>
2003-10-29 16:40:44 +0000

    Patch 6.2.136 Problem:    ":e ++enc=latin1 newfile" doesn't set 'fenc' when the file doesn't 	    exist.  (Miroslaw Dobrzanski-Neumann) Solution:   Set 'fileencoding' to the specified encoding when editing a file 	    that does not exist. Files:	    src/fileio.c

--- src/fileio.c
+++ src/fileio.c
@@ -543,6 +543,17 @@ readfile(fname, sfname, from, lines_to_skip, lines_to_read, eap, flags)
 		     * edited before and deleted.  Get the old marks. */
 		    check_marks_read();
 #endif
+#ifdef FEAT_MBYTE
+		    if (eap != NULL && eap->force_enc != 0)
+		    {
+			/* set forced 'fileencoding' */
+			fenc = enc_canonize(eap->cmd + eap->force_enc);
+			if (fenc != NULL)
+			    set_string_option_direct((char_u *)"fenc", -1,
+						    fenc, OPT_FREE|OPT_LOCAL);
+			vim_free(fenc);
+		    }
+#endif
 #ifdef FEAT_AUTOCMD
 		    apply_autocmds_exarg(EVENT_BUFNEWFILE, sfname, sfname,
 							  FALSE, curbuf, eap);
