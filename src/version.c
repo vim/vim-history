bdce85fff1d8dd12189e2dd119d5817dc300a1bb
Bram Moolenaar <bram@vim.org>
2004-03-15 17:25:58 +0000

    Patch 6.2.357 (after 6.2.321) Problem:    Memory leak when resizing the Vim window. Solution:   Free the LineWraps array. Files:	    src/screen.c

--- src/version.c
+++ src/version.c
@@ -638,12 +638,6 @@ static char *(features[]) =
 static int included_patches[] =
 {   /* Add new patch number below this line */
 /**/
-    360,
-/**/
-    359,
-/**/
-    358,
-/**/
     357,
 /**/
     356,
