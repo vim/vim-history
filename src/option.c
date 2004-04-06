8ae760fafc10504303be0f52fac8da785452d89e
Bram Moolenaar <bram@vim.org>
2004-04-06 04:37:41 +0000

    Patch 6.2.450 Problem:    "  #include" and "  #define" are not recognized with the default 	    option values for 'include' and 'defined'. (RG Kiran) Solution:   Adjust the default values to allow white space before the #. Files:	    runtime/doc/options.txt, src/option.c

--- src/option.c
+++ src/option.c
@@ -696,7 +696,7 @@ static struct vimoption
     {"define",	    "def",  P_STRING|P_ALLOCED|P_VI_DEF,
 #ifdef FEAT_FIND_ID
 			    (char_u *)&p_def, OPT_BOTH(PV_DEF),
-			    {(char_u *)"^#\\s*define", (char_u *)0L}
+			    {(char_u *)"^\\s#\\s*define", (char_u *)0L}
 #else
 			    (char_u *)NULL, PV_NONE,
 			    {(char_u *)NULL, (char_u *)0L}
@@ -1156,7 +1156,7 @@ static struct vimoption
     {"include",	    "inc",  P_STRING|P_ALLOCED|P_VI_DEF,
 #ifdef FEAT_FIND_ID
 			    (char_u *)&p_inc, OPT_BOTH(PV_INC),
-			    {(char_u *)"^#\\s*include", (char_u *)0L}
+			    {(char_u *)"^\\s*#\\s*include", (char_u *)0L}
 #else
 			    (char_u *)NULL, PV_NONE,
 			    {(char_u *)0L, (char_u *)0L}
