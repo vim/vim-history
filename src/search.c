6a19268abafa1ff67068f0fd420c63bf9fb6933a
Bram Moolenaar <bram@vim.org>
2004-04-23 08:09:58 +0000

    Patch 6.2.490 Problem:    With 'paragraph' it is not possible to use a single dot as a 	    paragraph boundary.  (Dorai Sitaram) Solution:   Allow using "  " (two spaces) in 'paragraph' to match ".$" or 	    ". $" Files:	    src/search.c

--- src/search.c
+++ src/search.c
@@ -2476,8 +2476,15 @@ inmacro(opt, s)
 
     for (macro = opt; macro[0]; ++macro)
     {
-	if (macro[0] == s[0] && (((s[1] == NUL || s[1] == ' ') &&
-		   (macro[1] == NUL || macro[1] == ' ')) || macro[1] == s[1]))
+	/* Accept two characters in the option being equal to two characters
+	 * in the line.  A space in the option matches with a space in the
+	 * line or the line having ended. */
+	if (       (macro[0] == s[0]
+		    || (macro[0] == ' '
+			&& (s[0] == NUL || s[0] == ' ')))
+		&& (macro[1] == s[1]
+		    || ((macro[1] == NUL || macro[1] == ' ')
+			&& (s[0] == NUL || s[1] == NUL || s[1] == ' '))))
 	    break;
 	++macro;
 	if (macro[0] == NUL)
