181b58f9c2df304979ba8b61cc82a6f147dc1454
Bram Moolenaar <bram@vim.org>
2005-10-01 00:32:57 +0000

    Patch 6.3.090 Problem:    A very big value for 'columns' or 'lines' may cause a crash. Solution:   Limit the values to 10000 and 1000. Files:	    src/option.c

--- src/option.c
+++ src/option.c
@@ -6821,6 +6821,11 @@ set_num_option(opt_idx, varp, value, errbuf, opt_flags)
 	}
 	Columns = MIN_COLUMNS;
     }
+    /* Limit the values to avoid an overflow in Rows * Columns. */
+    if (Columns > 10000)
+	Columns = 10000;
+    if (Rows > 1000)
+	Rows = 1000;
 
 #ifdef DJGPP
     /* avoid a crash by checking for a too large value of 'columns' */
