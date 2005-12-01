ea42ed9cc984ec9ce914e8107be52eeade059e5f
Bram Moolenaar <bram@vim.org>
2005-12-01 16:43:07 +0000

    Patch 6.4.004 Problem:    "go" does not work correctly in combination with 'virtualedit'. 	    (Alojz Motesicky) Solution:   Reset the "coladd" field. Files:	    src/memline.c

--- src/memline.c
+++ src/memline.c
@@ -4368,6 +4368,9 @@ goto_byte(cnt)
     {
 	curwin->w_cursor.lnum = lnum;
 	curwin->w_cursor.col = (colnr_T)boff;
+# ifdef FEAT_VIRTUALEDIT
+	curwin->w_cursor.coladd = 0;
+# endif
 	curwin->w_set_curswant = TRUE;
     }
     check_cursor();
